83c839420b48318eafaee0241b0fbd589e40aecd
amelie darnat <darnat.amelie@gmail.com>
2021-11-12 16:29:05 +0100

    jhkbhj

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -13,3 +15,15 @@ Movie.create(title: 'Titanic',
              overview: '101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.', poster_url: 'https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg', rating: 7.9)
 Movie.create(title: "Ocean's Eight",
              overview: 'Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.', poster_url: 'https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg', rating: 7.0)
+
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?&language=fr-FR'
+
+
+movies = JSON.parse(URI.open(url).read)
+
+
+movies["results"].each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], rating: movie["vote_average"],
+               poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}")
+end

12bb94d8e51bb178497664f71cce314145dde235
amelie darnat <darnat.amelie@gmail.com>
2021-11-12 12:15:07 +0100

    fkl

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,6 +31,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:comment, :name)
+    params.require(:list).permit(:comment, :name, :photo)
   end
 end
